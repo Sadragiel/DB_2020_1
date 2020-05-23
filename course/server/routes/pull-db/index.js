3cf8d7462e20e1fabd90560879c66ecc7be4c832
Danylo Hulko <danylo_hulko@epam.com>
2020-05-23 04:04:23 +0300

    backup

--- course/server/routes/pull-db/index.js
+++ course/server/routes/pull-db/index.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable prefer-template */
 const express = require('express');
 const axios = require("axios");
+const path = require('path');
+const child_process = require('child_process');
 const heroesDB = require('../../models/Hero');
 const urlBuilder = require('./../../utils/url/api-url');
 
@@ -29,8 +33,15 @@ async function equalizeHeroesForDbAndApi() {
     );
 }
 
-router.get('/db/pull', async (req, res) => {
+router.get('/db/backup', async (req, res) => {
     await equalizeHeroesForDbAndApi();
+    res.sendStatus(200);
+});
+
+router.get('/db/backup', async (req, res) => {
+    const pathToSave = path.join(__dirname, `./backup/${Date.now()}`);
+    const command = `mongodump --host localhost --port 27017 --db dota --out ${pathToSave} `;
+    child_process.exec(command);
     res.sendStatus(200);
 });
 
